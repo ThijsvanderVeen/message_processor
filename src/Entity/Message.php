8a06ccec944476f02613a44172ba5796bc654c79
tvanderveenalfa <t.vanderveen03@student.alfa-college.nl>
2024-05-20 17:07:07 +0200

    created entities

---
+++ src/Entity/Message.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $subject = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $body = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sender = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $processed = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $handler = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getSender(): ?string
+    {
+        return $this->sender;
+    }
+
+    public function setSender(string $sender): static
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getProcessed(): ?\DateTimeInterface
+    {
+        return $this->processed;
+    }
+
+    public function setProcessed(?\DateTimeInterface $processed): static
+    {
+        $this->processed = $processed;
+
+        return $this;
+    }
+
+    public function getHandler(): ?string
+    {
+        return $this->handler;
+    }
+
+    public function setHandler(?string $handler): static
+    {
+        $this->handler = $handler;
+
+        return $this;
+    }
+}
